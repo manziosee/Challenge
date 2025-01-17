cd03442aacbe374b696228ee53cfceea4dd673ae
manzi osee <85952735+manziosee@users.noreply.github.com>
2025-01-17 23:09:51 +0200

    Revert "feat: Initial commit for TaskForce-Challenge backend"

--- taskforce-backend/dist/utils/csvExport.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateCSV = void 0;
-const json2csv_1 = require("json2csv");
-const generateCSV = (data) => {
-    const parser = new json2csv_1.Parser();
-    return parser.parse(data);
-};
-exports.generateCSV = generateCSV;
